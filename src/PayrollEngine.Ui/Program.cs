ae3e502259156f6a78fcba28689ea5b92221f8c2
gökhan <kavazlikaya@gmail.com>
2026-04-03 13:22:35 +0300

    init42

--- src/PayrollEngine.Ui/Program.cs
+++ src/PayrollEngine.Ui/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PayrollEngine.Ui;
+using System.Globalization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,5 +9,10 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // API Base URL (geliştirme ortamında)
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5260/") });
+
+// Türkçe kültür ayarları (sayılar: 33.000,12 formatında gösterilecek)
+var culture = new CultureInfo("tr-TR");
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
 
 await builder.Build().RunAsync();
